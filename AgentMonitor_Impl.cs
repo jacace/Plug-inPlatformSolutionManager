2936a5f3668a06dfc5642c3b33bdd3a76f3a5264
jacace <javierandrescaceres@gmail.com>
2015-08-06 03:19:36 +0100

    Initial upload

---
+++ AgentMonitor_Impl.cs
@@ -0,0 +1,54 @@
+﻿using Intel.Ucrd.SolutionManagerAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plugin_AgentMonitor
+{
+    /// <summary>
+    /// Class to integrate via abstraction the AgentMonitor plug in with 
+    /// the PlugIn Manager/API provided by PSM
+    /// </summary>
+    public class AgentMonitor_Impl : SolutionManagerPlugin
+    {
+        ctrlAgentMonitor ctrl = null;
+
+        public override void ExecuteNoUI() { }
+
+        public AgentMonitor_Impl()
+        {
+            _properties.Icon = Plugin_AgentMonitor.Resource1.AgentIcon;
+            _properties.Name = "Agent Monitor";
+            _properties.Category = Categories.MISC | Categories.AMT;
+        }
+
+        public override bool RequiresAMT
+        {
+            get
+            {
+                return true;
+            }
+        }        
+
+        public override System.Windows.Controls.UserControl PluginWPFControl
+        {
+            get
+            {
+                lock (this)
+                {
+                    if (ctrl == null)
+                    {
+                        ctrl = new ctrlAgentMonitor(this);
+                    }
+                }
+                return ctrl;
+            }
+        }
+
+        public override SolutionManagerPlugin CreateInstance()
+        {
+            return new AgentMonitor_Impl();
+        }
+    }
+}
---
+++ ctrlAgentMonitor.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+using Intel.Ucrd.SolutionManagerAPI;
+using System.Xml.Serialization;
+using System.IO;
+//using Intel.Ucrd.vProPlatformSolutionManager;
+using Intel.Manageability.AgentPresence;
+using System.Collections.Generic;
+using Intel.Manageability.WSManagement;
+using Common;
+
+namespace Plugin_AgentMonitor
+{
+    /// <summary>
+    /// Interaction logic for ctrlAgentMonitor.xaml
+    /// </summary>
+    public partial class ctrlAgentMonitor : ctrlWPFBase
+    {
+
+        public ctrlAgentMonitor()
+        {
+            InitializeComponent();
+        }
+
+        public ctrlAgentMonitor(SolutionManagerPlugin plugin)
+            : base(plugin)
+        {
+            InitializeComponent();
+            _plugin = plugin;
+        }
+
+        private bool _isLoaded = false;
+        private void ctrlWPFBase_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!_isLoaded)
+            {
+                //One time stuff here
+                ListWatchdogs();
+            }
+            _isLoaded = true;
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            ListWatchdogs();
+        }
+
+        /// <summary>
+        /// Function to load a list of watch dogs
+        /// </summary>
+        private void ListWatchdogs()
+        {
+            IWSManClient wsmanClient = new DotNetWSManClient(_plugin.System.Fqdn, "admin", _plugin.System.Password,
+                                                             _plugin.System.UseTls, false, null, null);
+            WatchdogWrapper client = new WatchdogWrapper();
+            var list = client.List(wsmanClient, null);            
+            if (list != null)
+            {
+                lstAgents.DataContext = list;
+            }
+
+            
+        }        
+    }
+    
+}
